bbf8ab6218bbfed2a0f014c54c513f26df213b18
Stepan Yakovenko <stiv.yakovenko@mail.ru>
2025-03-15 21:19:40 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,8 @@
 apt-get update -y 
 apt-get install -y  python3-pip  python3-dev   git  build-essential  
+cd /workspace
+git clone https://github.com/cocktailpeanut/fluxgym
+cd fluxgym
+git clone -b sd3 https://github.com/kohya-ss/sd-scripts
+cd /workspace/fluxgym/models/unet
+wget https://huggingface.co/cocktailpeanut/xulf-dev/resolve/main/flux1-dev.sft
